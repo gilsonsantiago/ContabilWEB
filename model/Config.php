4fed2de3b81ec036cfbf811a5e8ae0fcf64843c6
Gilson Santiago <gs.thiago@hotmail.com>
2019-12-06 22:35:16 -0300

    Sim
    Erro

--- model/Config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    define( 'MYSQL_HOST', 'db4free.net' );
-    define( 'MYSQL_USER', 'pmmontadas2019' );
-    define( 'MYSQL_PASSWORD', '032293070' );
-    define( 'MYSQL_DB_NAME', 'protocolos' );
-
-?>
